cf06f029a4fe36ef8e10b4e9afbf8a23e350976d
phobiakalian <phobiakalian@gmail.com>
2025-06-22 04:11:15 +0700

    .

--- yn/plugins/start.py
+++ yn/plugins/start.py
@@ -3,6 +3,6 @@ from hydrogram import Client, filters
 from hydrogram.types import Message
 
 
-@bot.on_message(filters.command("start") & filters.group)
+@Client.on_message(filters.command("start") & filters.group)
 async def cmd_stats(client: Client, msg: Message):
     await client.send_messahe(msg.chat.id, "ppk")
